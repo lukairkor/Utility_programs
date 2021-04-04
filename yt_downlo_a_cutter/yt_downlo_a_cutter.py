07adc2f54164e232e96b48f9916b9844fe297295
Łukasz <lukasz.kordon@outlook.com>
2021-04-05 00:55:02 +0200

    handling exeptions and menu update

--- yt_downlo_a_cutter/yt_downlo_a_cutter.py
+++ yt_downlo_a_cutter/yt_downlo_a_cutter.py
@@ -5,8 +5,9 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import enquiries
 import os
-from datetime import datetime, date, time, timedelta
+import datetime
 import re
+import sys
 
 # information about video
 def video_info(youtube):
@@ -14,36 +15,28 @@ def video_info(youtube):
     print('\nVideo description: ', youtube.description)
     print('Rating', youtube.rating)
     print('Length', conversion )
-    print('Views', youtube.views, "\n")
-    
+    print('Views', youtube.views, "\n") 
+# https://www    
 # downloading video
-def down_yt_vid(url, path):
-    youtube = pytube.YouTube(url)
-    
-    video_info(youtube)
-    
+def down_yt_vid(youtube, path):
+  
     video = youtube.streams.filter().first().download(path)
     options = ['Yes', 'No']
     choice = enquiries.choose('Do you want rename file?: ', options)
     if choice == options[0]:    
-        print("Enter new name:")
-        new_name = input()
+        new_name = input("Enter new name:")
         os.rename(video, new_name +'.mp4')
     elif choice == options[1]:  
         pass
     
 # downloading only audio   
-def down_yt_vid_as_mp3(url, path):
-    youtube = pytube.YouTube(url)
+def down_yt_vid_as_mp3(youtube, path):
     
-    video_info(youtube)
-        
     video = youtube.streams.filter(only_audio=True).first().download(path)
     options = ['Yes', 'No']
     choice = enquiries.choose('do you want rename file?: ', options)
     if choice == options[0]:    
-        print("Enter new name:")
-        new_name = input()
+        new_name = input("Enter new name:")
         os.rename(video, new_name +'.mp4')
     elif choice == options[1]:       
         pass
@@ -54,7 +47,6 @@ def time_conver(time_tex):
     h = int(x[0]) * 3600
     m = int(x[1]) * 60
     s = int(x[2])
-
     suma = h + m + s
     return(suma)
     
@@ -81,35 +73,48 @@ def cropp_video(name, path):
     start = input()
     start = time_conver(start)
 
-    print("The end:")
-    stop = input()
+    stop = input("The end:")
     stop = time_conver(stop)
     
     ffmpeg_extract_subclip(source_path, start, stop/2, targetname = destin_path)
     
 # main loop
 if __name__ == "__main__":
+    assert ('linux' in sys.platform), "This code runs on Linux only."
     path = os.getcwd()
-    options = ['Download Video', 'Download only sound', 'Crop file', 'Close']
-
+    
+    options = ['Download Video', 'Crop file', 'Close']
     while(True):
         os.system('clear')
+
         choice = enquiries.choose('Choose one of these options: ', options)
-        print(choice)
         if choice == options[0]:
+            url = input("Enter url of video:\n")
             try:
-                url = input("Enter url of video")
-                down_yt_vid(url, path)
-            except:
-                print('Try again..')           
+                youtube = pytube.YouTube(url)
+                video_info(youtube)
+                options = ['Download Video', 'Download only sound', 'Back']
+                choice = enquiries.choose('Choose one of these options: ', options)
+                print(choice)    
+                if choice == options[0]:
+                    down_yt_vid(youtube, path)
+                elif choice == options[1]:
+                    down_yt_vid_as_mp3(youtube, path)  
+                elif choice == options[2]:
+                    continue
+            # This one should catch - pytube.exceptions.RegexMatchError:
+            except pytube.exceptions.RegexMatchError:
+              print('The Regex pattern did not return any matches for the video: {}'.format(url))      
+            except pytube.exceptions.ExtractError:
+              print ('An extraction error occurred for the video: {}'.format(url))      
+            except pytube.exceptions.VideoUnavailable:
+              print('The following video is unavailable: {}'.format(url))
+            input("\nPress any key to continue..")
         elif choice == options[1]:
-            url = input("Enter url of video")
-            down_yt_vid_as_mp3(url, path)  
-            continue
-        elif choice == options[2]:
-            name = input("Enter file name:")            
+            name = input("Enter file name:\n")            
             cropp_video(name, path)
-        elif choice == options[3]:
+        elif choice == options[2]:
             print("See you soon!")
             break
+
 
