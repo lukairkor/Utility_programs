649dd16ccc712260f2651d8c5784311df4643aee
Łukasz <lukasz.kordon@outlook.com>
2021-04-06 00:45:22 +0200

    merge into one function down_yt as vid and as mp3, downl progres bar, choose vieo format to downl

--- yt_downlo_a_cutter/yt_downlo_a_cutter.py
+++ yt_downlo_a_cutter/yt_downlo_a_cutter.py
@@ -8,6 +8,9 @@ import os
 import datetime
 import re
 import sys
+from moviepy.editor import preview
+from os.path import isfile, join
+from os import listdir
 
 # information about video
 def video_info(youtube):
@@ -16,14 +19,31 @@ def video_info(youtube):
     print('Rating', youtube.rating)
     print('Length', conversion )
     print('Views', youtube.views, "\n") 
+    
+    # chosing video format/available media formats
+    itagi = youtube.streams.filter(progressive=True).all()
+    print(itagi)
+    itag = input('Choose itag:\n')
+    yt1 = youtube.streams.get_by_itag(int(itag)) 
+    filesize = yt1.filesize
+    return filesize
 
+# calculate downloaded percent
+def progress(chunk, file_handle, bytes_remaining):
+    global filesize
+    os.system('clear')
+    remaining = (100 * bytes_remaining) / filesize
+    step = 100 - int(remaining)
+    print("Completed:", step) # show the percentage of completed download
+    
 # downloading video
 def down_yt_vid(youtube, path, kind):
-    if kind == 0:
+    if kind == 0:        
         video = youtube.streams.filter().first().download(path)
     elif kind == 1:
         video = youtube.streams.filter(only_audio=True).first().download(path)
     rename_file(video)
+    # video_play(video)
 
 # rename file
 def rename_file(video):
@@ -47,6 +67,7 @@ def time_conver(time_tex):
 
 # convert time from sec to h.m.s
 def time_conver_from_secon(clip):
+    # convert time from sec to h.m.s
     sec = clip.duration
     # mins = (clip.duration % 3600) / 60
     m, s = divmod(sec, 60)
@@ -54,10 +75,10 @@ def time_conver_from_secon(clip):
     m = str(int(m))
     h = str(int(h))
     s = str(int(s))
-    # conversion = datetime.timedelta(seconds=duration)
+
     print("Duration : "+h+"."+m+"."+s )
-    
-    print("\nInput begining and the end of file in seconds")
+    # convert back to seconds
+    print("\nInput begining and the end of file as: 0.0.0")
     print("Begining:")
     start = input()
     start = time_conver(start)
@@ -72,6 +93,7 @@ def cropp_video(name, path):
     
     options = ['Cut Video [only]: ', 'Cut Audio and convert to mp3']
     choice = enquiries.choose('Choose one of these options: ', options)
+    # cut only video into video
     if choice == options[0]:
         clip = VideoFileClip(source_path)  
         start, stop = time_conver_from_secon(clip)
@@ -83,7 +105,7 @@ def cropp_video(name, path):
         source_path = path + "/" + name
         clip.write_videofile(name + ".mp4", bitrate="4000k",
                               threads=1, preset='ultrafast', codec='h264')
-                
+    # cut audio and video into only audio           
     elif choice == options[1]:  
         clip = AudioFileClip(source_path)  
         start, stop = time_conver_from_secon(clip)
@@ -94,7 +116,13 @@ def cropp_video(name, path):
         clip.write_audiofile(name + ".mp3")
         
     input("\nPress any key to continue..")    
-    
+
+# preview video
+def video_play(name):  
+    clip = VideoFileClip(name)
+    # looping video 3 times
+    clip.preview(fps = 20)
+
 # main loop
 if __name__ == "__main__":
     assert ('linux' in sys.platform), "This code runs on Linux only."
@@ -105,20 +133,23 @@ if __name__ == "__main__":
         os.system('clear')
         choice = enquiries.choose('Choose one of these options: ', options)
         if choice == options[0]:
+            # url = "https://www.youtube.com/watch?v=hS5CfP8n_js&list=PLA-C_QHqX6ZkbVeCvNsq8P8isZP1J_KiB"
             url = input("Enter url of video:\n")
             try:
-                youtube = pytube.YouTube(url)
-                video_info(youtube)
-                options = ['Download Video', 'Download only sound', 'Back']
-                choice = enquiries.choose('Choose one of these options: ', options)
+                youtube = pytube.YouTube(url, on_progress_callback=progress)
+                
+                filesize = video_info(youtube)
+                # video_play(url)
+                optionss = ['Download Video', 'Download only sound', 'Back']
+                choice = enquiries.choose('Choose one of these options: ', optionss)
                 print(choice)    
-                if choice == options[0]:
+                if choice == optionss[0]:
                     down_yt_vid(youtube, path, 0)
-                elif choice == options[1]:
+                elif choice == optionss[1]:
                     down_yt_vid(youtube, path, 1)  
-                elif choice == options[2]:
+                elif choice == optionss[2]:
                     continue
-            # This one should catch - pytube.exceptions.RegexMatchError:
+            # pytube.exceptions.RegexMatchError:
             except pytube.exceptions.RegexMatchError:
               print('The Regex pattern did not return any matches for the video: {}'.format(url))      
             except pytube.exceptions.ExtractError:
@@ -127,7 +158,13 @@ if __name__ == "__main__":
               print('The following video is unavailable: {}'.format(url))
             input("\nPress any key to continue..")
         elif choice == options[1]:
-            file = input("Enter file name:\n")
+            # list files in folder
+            dirs = os.listdir(path)
+            for file in dirs:
+                if isfile(join(path, file)):
+                    print(file)
+            # type file name from above list
+            file = input("Enter file name from files above:\n")
             try:
                 open(file)
                 cropp_video(file, path)
@@ -136,5 +173,7 @@ if __name__ == "__main__":
         elif choice == options[2]:
             print("See you soon!")
             break
+        else:
+            continue
 
 
